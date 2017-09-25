8c3f407b8e39f0ffb6bacdc6861c9f90d693e980
Christopher Figueroa <christopher_figueroa@apple.com>
2017-09-25 16:31:42 -0700

    added unity project in with this

---
+++ Facepunch.Steamworks.Unity/Assets/Scripts/SteamTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Facepunch.Steamworks;
+
+public class SteamTest : MonoBehaviour
+{
+    private ServerList.Request serverRequest;
+    private Leaderboard leaderBoard;
+
+    private Vector2 achievementScrollPosition;
+
+
+    void Start ()
+	{
+        //
+        // Don't destroy this when loading new scenes
+        //
+        DontDestroyOnLoad( gameObject );
+
+        //
+        // Configure for Unity
+        // This is VERY important - call this before 
+        //
+	    Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
+
+        //
+        // Create the steam client using Rust's AppId
+        //
+        new Facepunch.Steamworks.Client( 252490 );
+
+        //
+        // Make sure we started up okay
+        //
+        if ( Client.Instance == null )
+        {
+            Debug.LogError( "Error starting Steam!" );
+            return;
+        }
+
+        //
+        // Request a list of servers
+        //
+	    {
+	        serverRequest = Client.Instance.ServerList.Internet();
+	    }
+
+        //
+        // Request a leaderboard
+        //
+	    {
+            leaderBoard = Client.Instance.GetLeaderboard( "TestLeaderboard", Client.LeaderboardSortMethod.Ascending, Client.LeaderboardDisplayType.Numeric );
+        }
+	}
+
+    private void OnDestroy()
+    {
+        if ( Client.Instance != null )
+        {
+            Client.Instance.Dispose();
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea( new Rect( 16, 16, Screen.width - 32, Screen.height - 32 ) );
+
+        if ( Client.Instance != null )
+        {
+            GUILayout.Label( "SteamId: " + Client.Instance.SteamId );
+            GUILayout.Label( "Username: " + Client.Instance.Username );
+
+            GUILayout.Label( "Friend Count: " + Client.Instance.Friends.AllFriends.Count() );
+            GUILayout.Label( "Online Friend Count: " + Client.Instance.Friends.AllFriends.Count( x => x.IsOnline ) );
+
+            //get the achievements
+            GUILayout.Label("============== Achievements  ==============");
+            GUILayout.Label("Achievement Count: " + Client.Instance.Achievements.All.Count() );
+            
+            //list all the achievements
+            foreach ( var result in Client.Instance.Achievements.All )
+            {
+                GUILayout.Label("- " + result.Name);
+            }
+
+            GUILayout.Label("============================");
+
+        
+        }
+        else
+        {
+            GUILayout.Label( "SteamClient Failed. Make sure appropriate files are in root, and steam is running and signed in." );
+        }
+
+        GUILayout.EndArea();
+    }
+
+    void Update()
+    {
+        if ( Client.Instance != null )
+        {
+            Client.Instance.Update();
+        }
+    }
+
+}
---
+++ Facepunch.Steamworks.Unity/Assets/Tests/AchievementsTest.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Facepunch.Steamworks;
+
+// public class AchievementsTest {
+
+// 	[Test]
+// 	public void AchievementsTestSimplePasses() {
+// 		// Use the Assert class to test conditions.
+// 	}
+
+// 	// A UnityTest behaves like a coroutine in PlayMode
+// 	// and allows you to yield null to skip a frame in EditMode
+// 	[UnityTest]
+// 	public IEnumerator AchievementsTestWithEnumeratorPasses() {
+// 		// Use the Assert class to test conditions.
+// 		// yield to skip a frame
+// 		yield return null;
+// 	}
+// }
+
+public class ClientTests
+    {
+
+		[UnityTest]
+        public IEnumerator GetUserName()
+        {
+			if ( Client.Instance == null )
+			{
+				Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
+				new Facepunch.Steamworks.Client( 252490 );
+				
+				yield return new WaitForSeconds(0.5f);
+			}
+			if ( Client.Instance == null )
+			{
+				Debug.LogError( "Error starting Steam!" );
+			}
+			
+			Assert.IsNotNull(Client.Instance.Username, "UserName Found");
+        }
+
+		[UnityTest]
+        public IEnumerator GetSteamID()
+        {
+			if ( Client.Instance == null )
+			{
+				Facepunch.Steamworks.Config.ForUnity( Application.platform.ToString() );
+				new Facepunch.Steamworks.Client( 252490 );
+				
+				yield return new WaitForSeconds(0.5f);
+			}
+
+			if ( Client.Instance == null )
+			{
+				Debug.LogError( "Error starting Steam!" );
+			}
+			
+			Assert.IsNotNull(Client.Instance.SteamId, "SteamID Found");
+        }
+
+    }
